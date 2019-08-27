a3c76b501b4f8ac78500b9c281c6ca531f020516
João Paulo <joaopaulojjjjjjj@hotmail.com>
2019-08-27 14:39:57 -0300

    Class Mascara
    foi criado a classe

---
+++ mascara.php
@@ -0,0 +1,31 @@
+<?php
+
+    class Mascara{
+        public function __construct($val, $mask) {
+            //////// VARIAVEIS DE INSTANCIA - CREDENCIAIS
+            $this->val = $val;
+            $this->mask = $mask;
+        }
+
+        /////////// CONEXAO POSTGRES
+        public function doMask(){
+            $mask = $this->mask;
+            $val = $this->val;
+
+            $maskared = '';
+            $k = 0;
+            for($i = 0; $i<=strlen($mask)-1; $i++) {
+                if(isset($val[$k])) {
+                    if($mask[$i] == '#') {
+                        if(isset($val[$k]))
+                            $maskared .= $val[$k++];
+                    } else {
+                        if(isset($mask[$i]))
+                        $maskared .= $mask[$i];
+                    }
+                }
+            }
+            return $maskared;
+        }
+
+    }
